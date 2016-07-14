48469727f73c3d5a51c832d76c61a0b218403150
Sergey Chernigin <Sergey_Chernigin@inbox.ru>
2016-07-14 18:26:56 +0500

    create calculator

---
+++ src/chernigin/CalcInit.java
@@ -0,0 +1,11 @@
+package chernigin;
+
+public class CalcInit{
+	public static void main(String[] agrs){
+		Calculator calculator = new Calculator();
+		calculator.add(2,2);
+		calculator.substruct(2,1);
+		calculator.div(3,3);
+		calculator.multiple(2,2);
+	}
+}
